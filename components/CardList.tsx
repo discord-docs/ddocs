68657a217bc9bc008b654213637fdffbef6ff8e9
megumin <megumin.bakaretsurie@gmail.com>
2022-02-01 20:56:30 +0000

    chore(cardslist): remove alignment (#27)

--- components/CardList.tsx
+++ components/CardList.tsx
@@ -6,10 +6,7 @@ const CardList = styled("div", {
   gridTemplateColumns: "repeat(auto-fit, minmax(250px, max-content))",
   gridTemplateRows: "290px",
   gap: 40,
-  marginLeft: "auto",
-  marginRight: "auto",
   width: "100%",
-  justifyContent: "center",
 });
 
 export default CardList;
