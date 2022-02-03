07af4c646a726f0ab934159a55c19b28a531ba4b
Quin Lynch <lynchquin@gmail.com>
2022-02-03 19:53:34 -0400

    fix build errors

--- components/EventSidebar.tsx
+++ components/EventSidebar.tsx
@@ -120,7 +120,7 @@ const EventSidebar: FunctionComponent<EventSidebarProps> = ({
           </>
         ) : (
           <NoItemsFound>
-            After searching around we couldn't find any events matching your
+            After searching around we couldn&apost find any events matching your
             query :(. Try searching with a different keyword instead!
           </NoItemsFound>
         )}
